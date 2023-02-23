cf729b985fc3b8ee54cc770ba2e0dc84397785ed
Alberto Fabbri <alberto_fabbri.git@yahoo.com>
2023-02-23 16:12:23 +0100

    Now it works with the web app

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 import { Server } from "socket.io";
 
-const io = new Server(1234);
+// Bypass CORS policy
+const io = new Server(1234, {
+  cors: {
+    origin: '*'
+  }
+});
 
 io.on("connection", (socket) => {
+
+  // Log when client connects
+  console.log("Client connected");
+
   // If this message is received then do something
   socket.on("open door", (...args) => {
-    console.log("Door opened xd")
-  })
+    console.log("Door opened xd");
+  });
 
   setInterval(() => {
-    const random = Math.floor((Math.random() * 1300) + 1)
-    console.log(random)
-    socket.emit("random", random) // Sends random number to client
-  }, 3000)
-})
+    const random = Math.floor((Math.random() * 1300) + 1);
+    // console.log(random);
+    socket.emit("random", random); // Sends random number to client
+  }, 3000);
+
+  // Log every received message
+  socket.onAny((event, ...args) => {
+    console.log(`got ${event}`);
+  });
+
+});
