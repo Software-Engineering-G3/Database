dba4867fff308bb0d1ef036ea7e831d2efa0a1ab
Fisnik <25180667+defunxyz@users.noreply.github.com>
2023-04-01 00:00:40 +0200

    Fixed player with additional support of handling.

--- components/player.js
+++ components/player.js
@@ -16,85 +16,62 @@ const Player = class {
         this.songs = args;
         this.current = null;
         this.state = PlayerState.Uninitialized;
-
-        console.log("length:" + this.length)
-        console.log("songs:" + this.songs)
     }
 
     async play(){   
         try {
-            // Create a new instance of Audic with the current song
-            this.current = new Audic(this.songs[this.index]);
-
-            // Play the current song
-            await this.current.play();
-            
-            console.log("Playing: " + this.current.src)
-
-            // Set the state of the player to Playing
-            this.state = PlayerState.Playing;
-
-            // When the current song ends, destroy the current instance of Audic
+            if(this.current.Paused) {
+                await this.current.play();
+                this.state = PlayerState.Playing;
+            }
+            else {
+                this.current = new Audic(this.songs[this.index]);
+                await this.current.play();
+                this.state = PlayerState.Playing;
+            }
             this.current.addEventListener("ended", () => { this.current.destroy(); });
         } catch (error) {
-            // If there is an error initializing the Audic instance, log the error and set the state of the player to Unitialized
             console.error(error);
             this.state = PlayerState.Uninitialized;
         }
     }
 
     async pause(){
-        // Check if the current song is playing
+        if(this.state == PlayerState.Paused) return;
         if(this.current.playing){
-            // Pause the current song
             this.current.pause();
-            
-            // Set the state of the player to Pause
             this.state = PlayerState.Paused;
         }
     }
 
     async stop(){
-        // Check if the current song is playing or if the player is in a paused state
+        if(this.state == PlayerState.Stopped) return;
         if(this.current.playing || this.state == PlayerState.Paused){
-            // Destroy the instance of Audic
             this.current.destroy();
-            
-            // Set the state of the player to Stopped
             this.state = PlayerState.Stopped;
         }
     }
     
     async prev(){
-        // Check if there is only one song in the list
         if(this.length == 1)
             return;
 
-        // Decrement the index of the current song
         --this.index;
-        
-        // If the index is less than 0, set it to the last song in the list
         if(this.index < this.length)
             this.index = 0;
 
-        // Stop the current song and start playing the new song at the updated index
         this.stop();
         this.play();
     }
 
     async next(){
-        // Check if there is only one song in the list
         if(this.length == 1)
             return;
         
-        // Increment the index of the current song
         ++this.index;
-        
-        // If the index is greater than or equal to the length of the list, set it to 0
         if(this.index >= this.length)
             this.index = 0;
         
-        // Stop the current song and start playing the new song at the updated index
         this.stop();
         this.play();
     }
